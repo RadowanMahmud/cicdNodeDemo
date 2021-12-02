edb79e8fdb904cf06a1572fec47d5bd2093e5451
RadowanMahmud <bsse1002@iit.du.ac.bd>
2021-12-03 05:43:10 +0600

    chane 1

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.port || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello !');
 });
 
 app.get('/add/:a/:b', (req, res) => {
